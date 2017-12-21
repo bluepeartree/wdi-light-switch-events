eebf0defb83b361b5710666bd26a5dfec01c8c37
bluepeartree <sherrie.sebastian@gmail.com>
2017-12-21 15:04:18 -0500

    light switch works except text

--- js/main.js
+++ js/main.js
@@ -1,2 +1,31 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+var body = document.querySelector('body');
+var button = document.querySelector('button');
+var message = document.querySelector('.status');
+
+button.addEventListener('click', function() {
+  if (button.classList.contains('on'))
+{
+  button.classList.toggle('off');
+  body.classList.toggle('dark');
+  message.innerText = "Hey, who turned off the lights?";
+
+}
+else {
+
+  button.classList.toggle('on');
+  body.classList.toggle('light');
+  message.innerText = "It\'s so bright in here!";
+}
 
 
+}
+
+
+)
+
+
+
+}
+)
